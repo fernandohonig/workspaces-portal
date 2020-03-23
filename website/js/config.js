74b3199f04c3a93630f279d89afd43db9acde118
Fernando Honig <fernando@deltahost.cloud>
2020-03-23 07:20:02 +0000

    New Deploy

--- website/js/config.js
+++ website/js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_5n6BV6hro', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3uqm770mp9rhfv5um3cu56vfau', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-west-1_qXGgijNLD', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '19e8bb1ejg8ejnmrenrt08upnn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://2fz6f9dm52.execute-api.eu-west-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://mzngy4zttk.execute-api.eu-west-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
         email: 'info@nubego.io'
