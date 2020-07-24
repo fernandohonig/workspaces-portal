79b9b837c1a19c912eecc151cb8cae4ea4232f00
Fernando Honig <fernando@nubego.io>
2020-07-24 17:10:02 +0100

    New Directory Id

--- website/js/config.js
+++ website/js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_qXGgijNLD', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '19e8bb1ejg8ejnmrenrt08upnn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-west-1' // e.g. us-east-2
+        userPoolId: 'eu-west-2_hj480fXZo', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '21631s98s0vt2m8tpqt61f0tsk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://mzngy4zttk.execute-api.eu-west-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://5ogxb9bsn4.execute-api.eu-west-2.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
         email: 'info@nubego.io'
